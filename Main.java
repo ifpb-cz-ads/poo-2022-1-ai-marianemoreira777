7a342853ed7ef11c889988ac7c83c5362ed74fe8
Mariane M. de Lacerda <96030731+marianemoreira777@users.noreply.github.com>
2022-05-14 19:20:28 -0300

    Add programa inicial java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("Digite o nome da musica que deseja reproduzir: ");
+    
+    String nomemusica = entrada.next();
+  }
+}
